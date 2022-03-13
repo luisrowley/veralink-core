d4245687da328cec70a8d6f232335a1d5e8f67a2
luisrowley <louisdebenito@gmail.com>
2022-03-13 19:09:32 +0100

    spring boot install complete

---
+++ src/test/java/com/veralink/core/VeralinkCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veralink.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeralinkCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
